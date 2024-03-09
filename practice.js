b369413ceb2e4251e06e4539223440876f16d78d
= <=>
2024-03-09 05:54:03 -0800

    conditional statement practice and logic building work.

--- practice.js
+++ practice.js
@@ -66,9 +66,56 @@
 // let numOne = +prompt("Enter first Number");
 // let numTwo = +prompt("Enter second Number");
 // let sum = numOne + numTwo;
-// if (numTwo % 10 > sum) {
+// if (numTwo % 10 < sum) {
 //     console.log("Hello");
 // }
 // else {
 //     console.log("Hey");
+// }
+
+// user sa do password manjo agar match kar jain tu login karo nahi tu mana karo.
+
+// let password = prompt("Enter your password");
+// let confirmpassword = prompt("Confirm your password");
+// if (password === confirmpassword) {
+//     console.log("Login successful!");
+// }
+// else {
+//     console.log("password not match");
+// }
+
+// check the number that user enter in promt is even or odd
+
+// let number = +prompt("Enter any number");
+// if (number % 2 == 0) {
+//     console.log("Even");
+// }
+// else {
+//     console.log("Odd");
+// }
+
+// let menu = prompt("tell me your order");
+// menu = menu.toLowerCase();
+// if (menu === "biryani") {
+//     console.log("Available");
+// }
+// else if (menu === "tikka") {
+//     console.log("Available");
+// }
+// else if (menu == "drink") {
+//     console.log("Available");
+// }
+// else {
+//     console.log("does not exist");
+// }
+
+// let randomNumber = +prompt("Enter the number");
+
+// if (randomNumber > 20) {
+//     if (randomNumber < 30 && randomNumber > 20) {
+//         console.log("number lies between 20 and 30");
+//     }
+// }
+// else {
+//     console.log(randomNumber)
 // }
